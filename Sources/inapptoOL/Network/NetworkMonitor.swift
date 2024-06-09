7842ed196785feb46e5adea822c40d0b09e27e30
Erkam KÜCET <erkam.kucet@external.yapikredi.com.tr>
2024-06-09 11:17:39 +0300

    Added NetworkMonit

---
+++ Sources/inapptoOL/Network/NetworkMonitor.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//
+//
+//  Created by Erkam Kucet on 9.06.2024.
+//
+
+import Foundation
+import Network
+
+internal final class NetworkMonitor: ObservableObject {
+    let monitor = NWPathMonitor()
+    let queue = DispatchQueue(label: "Monitor")
+    
+    @Published var isConnected = true
+    
+    init() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.isConnected = path.status == .satisfied ? true : false
+            }
+        }
+        monitor.start(queue: queue)
+    }
+}
